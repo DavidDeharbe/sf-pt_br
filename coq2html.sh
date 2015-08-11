7e4d791aef08ea0b106b8569b6c5ce594285d14c
David Deharbe <deharbe@gmail.com>
2015-08-11 09:49:38 -0300

    distribuicao tarefa traducao

--- coq2html.sh
+++ coq2html.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-coqdoc --html --with-header header.html --utf8 $1 --parse-comments --interpolate --no-index --no-glob
+coqdoc --html --with-header header.html --utf8 $1 --interpolate --no-index --no-glob --no-lib-name
